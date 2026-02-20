ae581dc8117063b1cbc4c5f6ebe8b42c5e389067
Suhaib Khan <suhaib0900@gmail.com>
2026-02-20 15:01:21 +0000

    updated config

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
-import cookieParser from 'cookie-parser';
+import { config } from "dotenv";
 config();
-import express from 'express';
-import { config } from 'dotenv';
-import cors from 'cors';
-import morgan from 'morgan';
-import errorMiddleware from './middlewares/error.middleware.js';
+
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+import morgan from "morgan";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
 
@@ -17,30 +18,30 @@ app.use(
   cors({
     origin: [process.env.FRONTEND_URL],
     credentials: true,
-  })
+  }),
 );
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cookieParser());
 
 // Server Status Check Route
-app.get('/ping', (_req, res) => {
-  res.send('Pong');
+app.get("/ping", (_req, res) => {
+  res.send("Pong");
 });
 
 // Import all routes
-import userRoutes from './routes/user.routes.js';
-import courseRoutes from './routes/course.routes.js';
-import paymentRoutes from './routes/payment.routes.js';
-import miscRoutes from './routes/miscellaneous.routes.js';
+import userRoutes from "./routes/user.routes.js";
+import courseRoutes from "./routes/course.routes.js";
+import paymentRoutes from "./routes/payment.routes.js";
+import miscRoutes from "./routes/miscellaneous.routes.js";
 
-app.use('/api/v1/user', userRoutes);
-app.use('/api/v1/courses', courseRoutes);
-app.use('/api/v1/payments', paymentRoutes);
-app.use('/api/v1', miscRoutes);
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/courses", courseRoutes);
+app.use("/api/v1/payments", paymentRoutes);
+app.use("/api/v1", miscRoutes);
 
 // Default catch all route - 404
-app.all('*', (_req, res) => {
-  res.status(404).send('OOPS!!! 404 Page Not Found');
+app.all("*", (_req, res) => {
+  res.status(404).send("OOPS!!! 404 Page Not Found");
 });
 
 // Custom error handling middleware
